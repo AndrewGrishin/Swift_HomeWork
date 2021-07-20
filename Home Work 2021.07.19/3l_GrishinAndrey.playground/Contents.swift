9658f64055b1884a7f64696eaf72b9e596cd71d4
AndreiGrishin <grishinandreyy@gmail.com>
2021-07-20 12:49:56 +0300

    Edited: Completed HW 2021.07.19

--- Home Work 2021.07.19/3l_GrishinAndrey.playground/Contents.swift
+++ Home Work 2021.07.19/3l_GrishinAndrey.playground/Contents.swift
@@ -1,13 +1,16 @@
 import UIKit
 
+// Опеределяем перечисление действи автомобиля
 enum CarActions {
     case openWindows, closeWindows
     case launchEngine, stopEngine
     case loadObject(Double), loadOutObject(Double)
 }
 
+// определеяем структуру автомобиля
 struct Car {
 
+    // перечисления EngineOnOff, WindowsOpenClose, CarType определены для удобства работы со структурой Car
     enum EngineOnOff {
         case on
         case off
@@ -17,6 +20,7 @@ struct Car {
         case opened, closed
     }
 
+    // Перечисление CarType создано, чтобы не копировать одинаковые свойства и методы структуры для разных типов машин
     enum CarType {
         case sportCar, trunkCar
     }
@@ -29,6 +33,7 @@ struct Car {
     private var engineStatus: EngineOnOff
     private var windowsStatus: WindowsOpenClose
     
+    // опциональный инициализатор
     init? (type: CarType, mark: String, releaseYear: Int, totalTrunckCapacity: Double) {
         guard releaseYear >= 0 && totalTrunckCapacity >= 0 else { return nil }
         self.type = type
@@ -42,12 +47,13 @@ struct Car {
     
     mutating func actionDetector(action: CarActions) {
         
+        // Блокирует функционал машины, когда двигатель выключен
         func engineIsOFF() -> Bool {
             if self.engineStatus == .off {
                 print("Nothing can be done with this car. Engine is OFF.")
-                return false
+                return true
             }
-            return true
+            return false
         }
         
         switch action {
@@ -109,6 +115,8 @@ struct Car {
         }
     }
     
+    // Так как все параметры private => ими нельзя воспользоваться вне структуры
+    // Значит,
     func getCarType() -> CarType {
         return self.type
     }
@@ -137,10 +145,52 @@ struct Car {
         return self.windowsStatus
     }
     
+    func showCarInfo() {
+        print("\nCar type -> \(self.type)")
+        print("Car mark -> \(self.mark)")
+        print("Car release year -> \(self.releaseYear)")
+        print("Car total trunk capacity -> \(self.totalTrunckCapacity)")
+        print("Car remained trunk capacity -> \(self.remainedTrunkCapacity)")
+        print("Car's engine status -> \(self.engineStatus)")
+        print("Car's windows status -> \(self.windowsStatus)\n\n")
+    }
 }
 
-var sportCar: Car? = Car(type: .trunkCar, mark: "Honda", releaseYear: 2017, totalTrunckCapacity: 1000)
+var sportCar = Car(type: .sportCar, mark: "Honda", releaseYear: 2017, totalTrunckCapacity: 1000)
+var trunkCar = Car(type: .trunkCar, mark: "Toyota", releaseYear: 2010, totalTrunckCapacity: 10_000)
+var luxuryCar = Car(type: .sportCar, mark: "Aston Martin", releaseYear: 2018, totalTrunckCapacity: 650)
+var wrongCar = Car(type: .sportCar, mark: "Honda", releaseYear: -2017, totalTrunckCapacity: -1000) // nil
 
-if var sportCar = sportCar {
+
+if var sportCar = sportCar, var trunkCar = trunkCar, var luxuryCar = luxuryCar {
+    sportCar.actionDetector(action: .openWindows)
+    sportCar.actionDetector(action: .launchEngine)
+    sportCar.actionDetector(action: .loadObject(200))
+    sportCar.actionDetector(action: .loadObject(2000))
+    sportCar.actionDetector(action: .loadObject(800))
+    sportCar.actionDetector(action: .loadOutObject(1000))
+    sportCar.actionDetector(action: .openWindows)
+    sportCar.actionDetector(action: .openWindows)
+    sportCar.actionDetector(action: .closeWindows)
+    sportCar.actionDetector(action: .stopEngine)
+    sportCar.showCarInfo()
     
+    trunkCar.actionDetector(action: .launchEngine)
+    trunkCar.actionDetector(action: .closeWindows)
+    trunkCar.actionDetector(action: .openWindows)
+    trunkCar.actionDetector(action: .loadObject(2000))
+    trunkCar.actionDetector(action: .loadOutObject(500))
+    trunkCar.actionDetector(action: .closeWindows)
+    trunkCar.actionDetector(action: .stopEngine)
+    trunkCar.showCarInfo()
+    
+    luxuryCar.actionDetector(action: .launchEngine)
+    luxuryCar.actionDetector(action: .openWindows)
+    luxuryCar.actionDetector(action: .loadObject(150))
+    luxuryCar.actionDetector(action: .loadObject(300))
+    luxuryCar.actionDetector(action: .loadOutObject(451))
+    luxuryCar.actionDetector(action: .loadOutObject(450))
+    luxuryCar.actionDetector(action: .closeWindows)
+    luxuryCar.actionDetector(action: .stopEngine)
+    luxuryCar.showCarInfo()
 }
